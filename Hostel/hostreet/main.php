2c1c4cacc6177f87cb53ef064ce67767411f615c
kamit2909 <43406393+kamit2909@users.noreply.github.com>
2019-09-23 04:43:31 -0700

    hostel project
    this repo is made for an internship task given by hostelary . In this i have made main page , google sign in authentication and other pages .

---
+++ Hostel/hostreet/main.php
@@ -0,0 +1,415 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Hostelary</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"  crossorigin="anonymous">
+ 
+
+
+
+
+
+</head>
+<body>
+<div class="image">
+<nav class="navbar navbar-inverse  navbar-dark" style="border-radius: 0; box-shadow:white;">
+
+  <div class="container-fluid" >
+    <div class="navbar-header"   >
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="https://sundaram-dubey.imgbb.com/" style="font-family: cursive;"><i>Hostelary</i></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active "><a href="#" style="font-family: cursive,italic;"  ><span class="glyphicon glyphicon-home">&nbsp;<i>Home</i></span></a></li>
+        
+        <li ><a href= "#teams"  style="font-family: cursive,italic;"><span class="glyphicon glyphicon-pencil" >&nbsp;<i>About-us</i></span></a></li>
+        <li    ><a href="#complain" style="font-family: cursive;"><span class="glyphicon glyphicon-envelope"> <i>Complain</i></span></a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span> profile</a></li>
+          <li><a href="register.php"><span class="glyphicon glyphicon-plus"></span> enhance-profile</a></li>
+        <li><a href="logout.php"><span class="glyphicon glyphicon-off"></span> Logout</a></li>
+      </ul>
+      <a class="navbar-brand"   href="https://hbtu.ac.in" style="font-family: cursive; float: right;"><i>Announcements</i></a>
+    </div>
+  </div>
+</nav>
+
+<center>
+  <div style="font-size: 60px;font-family:cursive;color: white;">Live a life at a place you want
+  </div>
+  <p style="color: white;font-family: inherit;">Find best hostel package from seller all across the world.</p>
+</center>
+
+
+
+<br><br><br><br><br><br><br>
+<section class="search-sec" style="  padding: 2rem;">
+    <div class="container">
+        <form action="#" method="post" novalidate="novalidate">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <input type="text" class="form-control search-slt" placeholder="Room and Guests">
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <input type="text" class="form-control search-slt" placeholder="Check-Out">
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <select class="form-control search-slt" id="exampleFormControlSelect1">
+                                <option>Enter City</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                            </select>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <button type="button" class="btn btn-danger wrn-btn">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+
+
+</center>
+
+
+
+
+
+
+</div>
+
+
+
+   <style>
+
+
+.image{
+  background: url(http://images2.fanpop.com/images/photos/7700000/Forest-wallpaper-twilight-series-7768791-847-479.jpg);
+  height: 500px;
+}
+
+
+
+
+ 
+.bottom-right {
+  position: absolute;
+  bottom: 1px;
+  right: 200px;
+  
+}
+
+</style>
+</head>
+<body> 
+
+ 
+  
+<!-- <div  style="margin-top: -100px">
+ 
+   <img src="https://images.unsplash.com/photo-1501139083538-0139583c060f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" width= "100%"  height="50%">
+</div> -->
+</body></div>
+<style type="text/css">
+
+.amit{
+  background-image: url("https://images.unsplash.com/photo-1501139083538-0139583c060f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80");
+}
+
+
+
+   @media only screen and (max-width: 700px) {
+    .h3{
+      font-size: 0px;
+      text-align: center;
+        background-color: none;
+
+           }
+           .p{
+            font-size: 0px;
+             background-color: none;
+           }
+    
+
+   }
+</style>
+ 
+
+
+
+
+
+ 
+ 
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+/* Style inputs */
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  margin-top: 6px;
+  border-radius: 10px;
+  margin-bottom: 16px;
+  resize: vertical;
+
+}
+
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+   opacity: .7;
+}
+
+/* Style the container/contact section */
+.container1
+ {
+  border-radius: 15px;
+  background-color: #f2f2f1;
+  padding: 10px;
+}
+
+/* Create two columns that float next to eachother */
+.column {
+  float: left;
+  width: 100%;
+  align-self: center;
+  margin-top: 6px;
+  padding: 20px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .column{
+    width: 100%;
+    margin-top: 0;
+  }
+}
+
+@media screen and (max-width: 600px) {
+   input[type=submit] {
+    width: 50%;
+    margin-top: 0;
+    border-radius: 5%;
+    background-color: darkblue;
+  }
+}
+
+</style>
+</head>
+<body>
+
+ 
+<div class="container1" id="complain">
+  <div style="text-align:center ; font-family: cursive;">
+    <h2><i>Feedback or Suggestion</i></h2>
+  </div>
+  
+    <div class="column" id="complain">
+      <form   method="post">
+        <label for="fname">Name</label>
+        <input type="text" id="fname" name="name" placeholder="Your name..">
+        <label for="lname">university enrollment number:</label>
+        <input type="text" id="lname" name="roll" placeholder="Enrollment no...">
+ <label for="lname">Branch :</label>
+        <input type="text" id="lname" name="branch" placeholder="Branch..">
+                
+
+
+<label>  your feedback/suggestion is related to :</label>
+ <input type="text" id="lname" name="feed" placeholder="Feedback">
+
+        
+
+
+
+
+        <label for="subject">Subject</label>
+        <textarea id="subject" name="subject" placeholder="Write your complain/Suggestion..." style="height:170px"></textarea>
+       <center>
+        <input type="submit" name="submit" value="Submit" style="border-radius: 100px ; margin-bottom: 20px">
+      </center> 
+ 
+ </form>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+<br><br>
+
+
+
+<div class="w3-container w3-padding-64 w3-center" id="teams">
+<h2>OUR TEAM</h2>
+<div class="w3-row"><br>
+
+<div class="w3-quarter">
+ <a href="https://ibb.co/c32JYBD"><img src="https://i.ibb.co/mThJNj6/Whats-App-Image-2019-04-25-at-22-30-18.jpg" alt="Whats-App-Image-2019-04-25-at-22-30-18" border="0" style="width:45%" class="w3-circle w3-hover-opacity"></a>
+  <h3>Amit Kumar</h3>
+  <p>Management Head </p>
+</div>
+
+<div class="w3-quarter">
+  <a href="https://ibb.co/b7qzBLN"><img src="https://i.ibb.co/b7qzBLN/Whats-App-Image-2019-04-25-at-20-55-32.jpg" alt="Whats-App-Image-2019-04-25-at-20-55-32" border="0" style="width:45%" class="w3-circle w3-hover-opacity"></a>
+  <h3>SUNDARAM DUBEY</h3>
+  <p>WEB,Django<BR></p>
+</div>
+
+<div class="w3-quarter">
+  <a href="https://ibb.co/THpHHQ8"><img src="https://i.ibb.co/zSjSS3R/Whats-App-Image-2019-04-25-at-22-44-00.jpg" alt="Whats-App-Image-2019-04-25-at-22-44-00" border="0" style="width:45%" class="w3-circle w3-hover-opacity" ></a>
+  <h3>VARUN SINGH</h3>
+  <p>ANDROID CERTIFIED</p>
+</div>
+
+<div class="w3-quarter">
+  <a href="https://ibb.co/THpHHQ8"><img src="https://i.ibb.co/zSjSS3R/Whats-App-Image-2019-04-25-at-22-44-00.jpg" alt="Whats-App-Image-2019-04-25-at-22-44-00" border="0" style="width:45%" class="w3-circle w3-hover-opacity" ></a>
+  <h3>VARUN SINGH</h3>
+  <p>ANDROID CERTIFIED</p>
+</div>
+
+<div class="w3-row"><br>
+
+
+
+
+   
+</div>
+ 
+</div>
+</div></div>
+
+
+<footer class="w3-container w3-padding-32 w3-theme-d1 w3-center">
+  <h4>Follow Us</h4>
+  <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Facebook"><i class="fa fa-facebook"></i></a>
+  <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Twitter"><i class="fa fa-twitter"></i></a>
+  <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Google +"><i class="fa fa-google-plus"></i></a>
+  <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Google +"><i class="fa fa-instagram"></i></a>
+  <a class="w3-button w3-large w3-teal w3-hide-small" href="javascript:void(0)" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+ 
+
+  <div style="position:relative;bottom:100px;z-index:1;" class="w3-tooltip w3-right">
+    <span class="w3-text w3-padding w3-teal w3-hide-small">Go To Top</span>   
+    <a class="w3-button w3-theme" href="#myPage"><span class="w3-xlarge">
+    <i class="fa fa-chevron-circle-up"></i></span></a>
+  
+  </div>
+  <div>
+  <p>copyright@hbtu2019</p></div>
+</footer>
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+<?php
+$conn = mysqli_connect("localhost", "root", "" , "sign-up"); 
+if(isset($_POST["submit"])){
+//$email = $_POST["email"];
+//$password= $_POST["password"];
+//$passwords = $_POST["passwords"];
+
+        $name  = mysqli_real_escape_string($conn,$_POST["name"]);
+        $roll     = mysqli_real_escape_string($conn,$_POST["roll"]);
+        $branch   = mysqli_real_escape_string($conn,$_POST["branch"]);
+        $feed = mysqli_real_escape_string($conn,$_POST["feed"]);
+        $subject = mysqli_real_escape_string($conn,$_POST["subject"]);
+
+
+
+
+$query = "SELECT 'roll' FROM `feedback` where roll='".$roll."'";
+        $result = mysqli_query($conn,$query); 
+        $numResults = mysqli_num_rows($result);
+        if($numResults>=1)
+        {
+            $message = " enrollment number already exist!!";
+        }
+        else
+        {
+          $quer = "INSERT INTO `feedback`(`name`, `roll`, `branch`, `feed`, `subject`)VALUES($name,$roll,$branch,$feed,$subject)";
+
+      if(mysqli_query($conn, $quer)){
+      echo "<script type='text/javascript'>alert('posted successfully!')</script>";
+        ///header("Location:localhost/hostreet/login.php");
+        header('profile.php');   
+      }else
+      {
+        echo "here is error";
+  }
+          
+        }
+  }
+
+?>
+
+
